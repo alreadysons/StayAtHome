6288ce66a1f052710909a1b1706c36f7dac16736
alreadysons <alreadysons@gmail.com>
2025-08-15 20:45:30 +0900

    오류 수정

--- backend/main.py
+++ backend/main.py
@@ -12,8 +12,6 @@ app.include_router(user)
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
