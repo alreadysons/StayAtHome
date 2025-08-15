c88868aee8b3573311d463e12b2dbd0ce155b68e
alreadysons <alreadysons@gmail.com>
2025-08-15 20:45:55 +0900

    유저 생성, 조회 API 생성

---
+++ backend/api/user_api.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+import models, schemas
+
+# 새로운 사용자를 생성
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(**user.model_dump())
+    
+    db.add(db_user)
+    db.commit()    
+    db.refresh(db_user)
+    return db_user
+
+# 전체 사용자 목록
+def get_all_users(db: Session, start: int = 0, last: int = 10):
+    return db.query(models.User).offset(start).limit(last).all()
+
+# 사용자 id로 사용자 반환
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+# 사용자 id로 사용자 반환
+def get_user_by_name(db: Session, user_name: str):
+    return db.query(models.User).filter(models.User.user_name == user_name).first()
--- backend/router/user.py
+++ backend/router/user.py
@@ -1,4 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+
+from database import get_db
+import schemas
+from api import user_api
 
 user = APIRouter(prefix="/user", tags=["user"])
+
+@user.post("/create", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = user_api.get_user_by_name(db, user.user_name)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    return user_api.create_user(db=db, user=user)
+
+@user.get("/name/{user_name}", response_model=schemas.User)
+def read_user_by_name(user_name: str, db: Session = Depends(get_db)):
+    db_user = user_api.get_user_by_name(db, user_name=user_name)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@user.get("/id/{user_id}", response_model=schemas.User)
+def read_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    db_user = user_api.get_user_by_id(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@user.get("/list", response_model=List[schemas.User])
+def read_users(start: int = 0, last: int = 10, db: Session = Depends(get_db)):
+    users = user_api.get_all_users(db, start=start, last=last)
+    return users
 
