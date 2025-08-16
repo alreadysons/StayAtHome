99c8af0c5a3e8609b4d14e5982c8f940a443fa7f
alreadysons <alreadysons@gmail.com>
2025-08-16 23:12:24 +0900

    Log Api service 추가

--- app/app/src/main/java/com/example/app/network/ApiService.kt
+++ app/app/src/main/java/com/example/app/network/ApiService.kt
@@ -1,9 +1,19 @@
 package com.example.app.network
 import com.example.app.data.UserCreateRequest
-import retrofit2.Response
+import com.example.app.data.UserResponse
+import com.example.app.data.StartLogRequest
+import com.example.app.data.LogResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Query
+
 interface ApiService {
     @POST("user/create")
-    suspend fun registerUser(@Body request: UserCreateRequest): Response<Unit>
+    suspend fun registerUser(@Body request: UserCreateRequest): UserResponse
+
+    @POST("log/start")
+    suspend fun startLog(@Body request: StartLogRequest): LogResponse
+
+    @POST("log/end")
+    suspend fun endLog(@Query("log_id") logId: Int): LogResponse
 }
